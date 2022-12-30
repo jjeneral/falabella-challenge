171e16b3f4c70c153f06f2bc96b303305830f2aa
Joshua Jeneral <joshua.jeneral@gmail.com>
2022-12-30 11:43:04 -0300

    feat: Add pattern regexp validation in sku field

--- src/main/java/com/jjeneral/falabella/challenge/mapper/ProductDtoMapper.java
+++ src/main/java/com/jjeneral/falabella/challenge/mapper/ProductDtoMapper.java
@@ -3,9 +3,10 @@ package com.jjeneral.falabella.challenge.mapper;
 import com.jjeneral.falabella.challenge.model.dto.ProductDto;
 import com.jjeneral.falabella.challenge.model.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.core.convert.converter.Converter;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductDtoMapper extends Converter<ProductDto, Product> {
     Product convert(ProductDto productDto);
 }
--- src/main/java/com/jjeneral/falabella/challenge/model/dto/ProductDto.java
+++ src/main/java/com/jjeneral/falabella/challenge/model/dto/ProductDto.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
@@ -14,6 +15,8 @@ import java.math.BigDecimal;
 public class ProductDto {
 
     @NotNull(message = "SKU must not be null" )
+    @Pattern(regexp = "^FAL\\-[1-9][0-9]{6,8}$",
+            message = "SKU must start with FAL- and then a value between 1000000 and 99999999")
     private String sku;
     @NotNull(message = "name must not be null" )
     @Size(min = 3,  message = "minimum name length must be 3")
